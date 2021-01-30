7c59139ba8bac4547da0c90b9c6468e8b84fdfba
junhaow1 <37771259+junhaow1@users.noreply.github.com>
2021-01-30 22:10:51 +1100

    re-structure the code, put into reusable section

--- projectAllinOne.py
+++ projectAllinOne.py
@@ -4,103 +4,97 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 import time 
 import re
-
 import csv
 import pandas as pd 
 import smtplib, ssl
-# from project import *
-# from project import linkList
-# from project import joinedList
 
 
 
-
-
+# future implement command line input 
 # priceNotification = float(input())
 # priceBottom = float(input())
 
-def project(priceNotification,priceBottom):
+def project(priceNotification,priceBottom,email_address,shopping_url):
 
-
+    #section 1
     #web scraping source 1 : individual websites 
+    # test purpose : in the future prepare for scraping static shopping websites like jb hifi or mwave
 
     URL = 'https://www.dell.com/en-au/shop/ultrasharp-27-4k-usb-c-monitor-u2720q/apd/210-auzu/monitors-monitor-accessories'
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.find('div',class_ = 'ps-dell-price')
     pricelist1 = re.findall("\d+\.\d+", results.text.strip())
     print(re.findall("\d+\.\d+", results.text.strip()))
 
-    # create a list to save href link to the product
+    # create a list to save href link (corresponding to each product from different websites)
     linkList = []
 
-    #add first url to the list
+    #add first url to the url list
     linkList.append(URL)
 
-
+    # sec specific static website 
     URL2 = 'https://www.umart.com.au/Dell-UltraSharp-27-4K-USB-C-Monitor--U2720Q_54620G.html'
     page2 = requests.get(URL2)
-
     soup = BeautifulSoup(page2.content, 'html.parser')
     results2 = soup.find('div',class_ = 'goods-price')
-
     pricelist2 = re.findall("\d+\.\d+", results2.text.strip())
     print(re.findall("\d+\.\d+", results2.text.strip()))
-    #add sec url to the list
+    #add sec url to the url list
     linkList.append(URL2)
 
 
 
-    #web scraping source 2 : google shopping price list
 
 
-    #wait to be automated , input parameter 
+
+    #section 2 
+    #web scraping source 2 : google shopping price table
+
+
+ 
     #url of the page we want to scrape 
-    # url="https://www.google.com/shopping/product/6377659988323960368?q=airpods+pro&sxsrf=ALeKk0132Gctf9nmdN05E4aI8iofBWPaeA:1611259968418&biw=1745&bih=881&prds=epd:13421449088386249105,prmr:1&sa=X&ved=0ahUKEwiLpLuR663uAhXN9nMBHVrsC2kQ3q4ECJoE#online"
-    url = "https://www.google.com/shopping/product/17116295002256069167?q=philips+air+fryer&sxsrf=ALeKk02xRkUSwF4paxvJHsJO6ZQtRR30zg:1611383913852&biw=1745&bih=881&prds=epd:6863085681753514412,prmr:1&sa=X&ved=0ahUKEwjzyLvvuLHuAhUbyDgGHYHlDmQQ3q4ECM4F#online"
+    url = shopping_url
     product_name = ""
 
-
-
-    # below is using selenium to 
-
-    # initiating the webdriver. Parameter includes the path of the webdriver. 
+    # codes below use selenium to initiate the webdriver. Parameter includes the path of the webdriver. 
     driver = webdriver.Chrome('./chromedriver')  
     driver.get(url)    
     # this is just to ensure that the page is loaded 
+    #wait for one sec
     time.sleep(1)   
     html = driver.page_source  
     # this renders the JS code and stores all of the information in static HTML code. 
-    
-    # Now, we could simply apply bs4 to html variable 
+
+
+
+    # Now, we could simply apply bs4 to html variable , like we always do
     soup = BeautifulSoup(html, "html.parser") 
     product_title = soup.find('span',{'class' : 'BvQan sh-t__title-pdp sh-t__title translate-content'})
     print(product_title.text)
     product_name = product_title.text
     priceTable = soup.find('table', {'id' : 'sh-osd__online-sellers-grid'}) 
     priceRow = priceTable.find_all('div', {'class' : 'sh-osd__total-price'}) 
-
     joinedList = pricelist1+pricelist2
 
-
-    #add href to the linkList
+    #get/scrape url
+    #add href to the url List
     urls = priceTable.find_all('a',{'class' :"internal-link ZvnyBe shntl"})
     base_url = 'https://www.google.com'
 
+    #add full url into the url list
     for eachLink in urls:
-        # we need to add base url in front of the link
+        # we need to add base url in front of each link
         linkList.append(base_url+eachLink['href'])
 
-
-    print(linkList)
+    print(linkList)  
     
 
+    #add each price in the google shopping table into price list
     count = 0
     for price in priceRow : 
         pricelist3 = re.findall("\d+\.\d+", price.text)
         joinedList += pricelist3
-
         print(re.findall("\d+\.\d+", price.text))
 
         count = count + 1
@@ -108,8 +102,6 @@ def project(priceNotification,priceBottom):
             break
     
     driver.close() # closing the webdriver 
-
-
     print(joinedList)
 
 
@@ -124,22 +116,20 @@ def project(priceNotification,priceBottom):
 
 
 
+    #put data(price and url list) into files we can process
 
-    # import price and url list from last py file
+
     # create a new pandas.df file . in columns , column name: price , link (url)
     data = {'price':joinedList,'link':linkList}
     df = pd.DataFrame(data,columns = ['price','link'])
-
-    # set a start original price for code
-    price_lowest = 950.00
+    # set a starting original price 
+    price_lowest = 100000.00
     price_lowest_link = ""
-    #this is the input parameter(threshold) , waiting to be automated!!!
-    # price_expectation = 450.00
+    #input
     price_expectation = priceNotification
-    # price_bottom = 200.00
     price_bottom = priceBottom
 
-
+    #simple algorithm: find cheapest one under our expectation price and above the bottom-price(avoid fake goods)
     #iterate df rows to find the lowest price under our input parameter(threshold)
     for index, row in df.iterrows():   
         if (  float(row["price"])<price_expectation  ):
@@ -153,12 +143,18 @@ def project(priceNotification,priceBottom):
     print(price_lowest_link)
 
 
-    # use SMTP to send email 
+
+
+    #section 3 : send email by SMTP
+
     port = 587  # For starttls
     smtp_server = "smtp.gmail.com"
     sender_email = "kevinwjh521@gmail.com"
     # input parameter
-    receiver_email = "kevinwjh520@gmail.com"
+    receiver_email = email_address
+    # receiver_email = "shouyinzuo@gmail.com"
+
+    
     # password = input("Type your password and press enter:")
     password = '1234554321qwer'
 
@@ -182,5 +178,5 @@ def project(priceNotification,priceBottom):
         server.sendmail(sender_email, receiver_email, message)
 
 
-
-project(450,200)
+#run the project file 
+project(350,250,"kevinwjh520@gmail.com","https://www.google.com/shopping/product/6377659988323960368?q=airpods+pro&sxsrf=ALeKk033IeTPP_O8Qh59eyJbcJhtNhWzbw:1611979747124&biw=1745&bih=881&prds=epd:17385103026593850038,prmr:1&sa=X&ved=0ahUKEwjFz47M5MLuAhUyzjgGHUzGBEMQ2SsIGw#online")
